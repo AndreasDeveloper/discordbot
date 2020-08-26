e3a1e553e4de63de57a231ca1d8a97ad53c5bfa4
AndreasDEV <andreasmrdja16@gmail.com>
2020-08-27 01:36:38 +0200

    Updated adminfunc file.

--- src/js/bot/adminfunc.js
+++ src/js/bot/adminfunc.js
@@ -2,22 +2,39 @@
 const prefix = '$';
 
 // Kick member
-function manipulateMember(message) {
+async function manipulateMember(message) {
     // Destructure command user inserts & trim whitespace
     const [cmd_name, ...args] = message.content.trim().substring(prefix.length).split(/\s+/);
 
     // Kick member functionality
     if (cmd_name === 'kick') {
+        // Checks
+        if (!message.member.hasPermission('KICK_MEMBERS')) return message.reply('You do not have permission.');
         if (args.length === 0) return message.reply('Provide an ID.');
+
         const member = message.guild.members.cache.get(args[0]);
         if (member) {
-            member.kick('Kicked because why not.');
-            message.channel.send('Kicked because why not.');
+            member.kick('Kicked because why not.').then(res => {
+                message.channel.send(`${member} was kicked, because why not.`);
+            }).catch(err => {
+                message.channel.send('No permission.');
+            });
         } else {
             message.channel.send('Member not found.');
         }
+    // Ban member functionality
     } else if (cmd_name === 'ban') {
-        message.channel.send('Banned');
+        // Checks
+        if (!message.member.hasPermission('BAN_MEMBERS')) return message.reply('You do not have permission.');
+        if (args.length === 0) return message.reply('Provide an ID.');
+        
+        try {
+            const user = await message.guild.members.ban(args[0]);
+            message.channel.send(`${user} was banned, because i say so.`);
+        } catch (err) {
+            console.log(err);
+            message.channel.send(err.message);
+        }
     }
 }
 
